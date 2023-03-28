66187cbb134bc2cf97638ad15281f26426c5d06f
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-03-28 13:46:03 +0300

    card updated

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -5,7 +5,7 @@ function Card({ user }: { user: User }) {
       <div className="w-[300px] bg-white h-[250px] overflow-auto  hover:shadow-xl  border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <div className="p-5 flex flex-col">
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            {user?.name} {user?.username}
+            {user?.name}
           </h5>
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
             {user?.phone}
@@ -18,7 +18,7 @@ function Card({ user }: { user: User }) {
           </p>
           <Link
             href={`/userpost/${user?.id}`}
-            className="mt-0 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="mt-3 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Read more
           </Link>
