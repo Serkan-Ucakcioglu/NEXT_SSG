0c1b95bc4ca17821d60361acf1910b74062c6e2a
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-03-28 13:30:33 +0300

    a delete

--- app/components/Post.tsx
+++ app/components/Post.tsx
@@ -5,11 +5,10 @@ function Post({ post }: { post: POST }) {
     <div className="p-4">
       <div className="max-w-sm h-[270px] overflow-auto  hover:shadow-xl  border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <div className="p-5">
-          <a href="#">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              {post?.title}
-            </h5>
-          </a>
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {post?.title}
+          </h5>
+
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
             {post?.body}
           </p>
