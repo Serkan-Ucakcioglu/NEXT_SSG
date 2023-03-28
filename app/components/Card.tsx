fecce3cdd6e001f11b529bbbc10138aa0b17123d
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-03-28 13:32:10 +0300

    update

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -22,7 +22,7 @@ function Card({ user }: { user: User }) {
             {user?.phone}
           </p>
           <Link
-            href={`/${user?.id}`}
+            href={`/userpost/${user?.id}`}
             className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Read more
--- app/userpost/[id]/page.tsx
+++ app/userpost/[id]/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { getAllUsers, getUser, getUserPost } from "../../lib/user";
+import { getAllUsers, getUser, getUserPost } from "../../../lib/user";
 import type { Metadata } from "next";
-import Post from "../components/Post";
+import Post from "../../components/Post";
 
 export async function generateMetadata({
   params: { id },
@@ -12,12 +12,14 @@ export async function generateMetadata({
   return { title: user.name };
 }
 
-async function Detail({ params: { id } }: { params: { id: String } }) {
+export default async function Detail({
+  params: { id },
+}: {
+  params: { id: String };
+}) {
   const userData: Promise<User> = getUser(id);
   const postData: Promise<POST[]> = getUserPost(id);
-
   const [user, posts] = await Promise.all([userData, postData]);
-
   return (
     <div className="p-8">
       <h1 className="flex justify-center text-center text-bold text-2xl ">
@@ -31,8 +33,6 @@ async function Detail({ params: { id } }: { params: { id: String } }) {
     </div>
   );
 }
-
-export default Detail;
 
 export async function generateStaticParams() {
   const data: Promise<User[]> = getAllUsers();
